5f7302393e1098504d1e885f4e62dd1fe3602e21
pawin21 <pawinstiu@gmail.com>
2016-04-17 18:40:53 +0700

    week2_homework

--- Week2/Week2/js/shop.js
+++ Week2/Week2/js/shop.js
@@ -1,24 +1,59 @@
+var myDateElement= document.getElementById("dateTime");
+var myDate = new Date(); 
+ 
+  
+	if (myDate.getHours() < 12 )  
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Morning,"
+} 
+		else 
+	if (myDate.getHours() >= 12 && myDate.getHours() <= 17 ) 
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Afternoon,"
+} 
+		else  
+	if (myDate.getHours() > 17 && myDate.getHours() <= 24 ) 
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Evening,"
+} 
+		else 
+{ 
+    document.write("I'm not sure what time it is!"); 
+} 
+
+
 var product =[
 	"Brooklyn T-shirt White",
 	"Brooklyn T-shirt Black",
-	"Apple WAtch",
+	"Apple Watch",
 	"Android Phone"
 ];
 var prices = [10,10,199,159];
 var productText ="";
+var discountPrice = 0
 var productElement = document.getElementById("product-list");
 
 productText += "<li class='list-group-item'><span class='badge'>$" + prices[0]+"</span>" + product[0]+"</li>";
 productText += "<li class='list-group-item'><span class='badge'>$" + prices[1]+"</span>" + product[1]+"</li>";
-productText += "<li class='list-group-item'><span class='badge'>$" + prices[2]+"</span>"  + product[2]+"</li>";		
+productText += "<li class='list-group-item'><span class='badge'>$" + prices[2]+"</span>" + product[2]+"</li>";		
 productText += "<li class='list-group-item'><span class='badge'>$" + prices[3]+"</span>" + product[3]+"</li>";
 
 productElement.innerHTML = productText;
 
 totalPrice = prices[0]+prices[1]+prices[2]+prices[3];
+discountPrice = totalPrice*0.75
 
-var totalPriceElement = document.getElementById("total-price");
-totalPriceElement.textContent=totalPrice;
+
+
+//var totalPriceElement = document.getElementById("total-price");
+//totalPriceElement.textContent=totalPrice;
+
+var discountPriceElement = document.getElementById("discount-price");
+discountPriceElement.textContent=discountPrice;
+
 
 var customerName="Pawin";
 var price= 10;
@@ -27,19 +62,4 @@ var quantity= 2;
 var customerElement = document.getElementById("customer-name");
 customerElement.textContent = customerName;
 
-function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-
-function myFunction() {
-    var d = new Date();
-    var x = document.getElementById("demo");
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    var s = addZero(d.getSeconds());
-    x.innerHTML = h + ":" + m + ":" + s;
-}
 
---
+++ Week3-2/js/task1.js
@@ -0,0 +1,29 @@
+(function(){
+	var task1 = {
+	render: function(){
+		document.getElementById("screenX").textContent = window.screenX;
+		document.getElementById("screenY").textContent = window.screenY;
+		document.getElementById("screenWidth").textContent = window.innerWidth;
+		document.getElementById("screenHeight").textContent = window.innerHeight;
+		document.getElementById("location").textContent = window.location.pathname;
+	},
+	resize: function() {
+		window.addEventListener("resize", this.render);
+
+	},
+	devInfo: function() {
+		var btnDev = document.getElementById("dev-info");
+		btnDev.addEventListener("click", function() {
+		document.getElementById("dev-info");
+		document.write("Pawin Abdulla");
+		document.write("IT Student");
+		document.write("");
+		});
+	}
+}
+
+task1.render();
+task1.resize();
+task1.devInfo();
+
+})();
