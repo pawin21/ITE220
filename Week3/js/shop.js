6905f19e295a8e0e4d884458202a0b32cabf2f7b
pawin21 <pawinstiu@gmail.com>
2016-05-23 10:36:25 +0700

    week3-1 done!!!!

--- Week3/js/shop.js
+++ Week3/js/shop.js
@@ -1,43 +1,63 @@
-var shop = {
-	customerName: "Pawin",
-	totalPrice: 0,
-	products: [
-		"Brooklyn T-shirt White",
-		"Brooklyn T-shirt Black",
-		"Apple Watch",
-		"Android Phone"
-	],
-	prices: [10,10,199,159],
-
-	displayCustomerName: function() {
-		var customerElement = document.getElementById("customer-name");
-		customerElement.textContent = this.customerName;
-	},	
-		
-	displayProductList: function(){
-		var productsText ="";
-		var productsElement = document.getElementById("product-list");
-
-		productsText += "<li class='list-group-item'><span class='badge'>$" + this.prices[0]+"</span>" + this.products[0]+"</li>";
-		productsText += "<li class='list-group-item'><span class='badge'>$" + this.prices[1]+"</span>" + this.products[1]+"</li>";
-		productsText += "<li class='list-group-item'><span class='badge'>$" + this.prices[2]+"</span>" + this.products[2]+"</li>";		
-		productsText += "<li class='list-group-item'><span class='badge'>$" + this.prices[3]+"</span>" + this.products[3]+"</li>";
-
-		productsElement.innerHTML = productsText;
-	}
-	
-	//calculateTotalPrice: function() {
-	//	return (this.prices[0]+this.prices[1]+this.prices[2]+this.prices[3]*0.75;
-	//},
-
-	//displayTotalPrices: function() {
-	//	this.totalPrice = this.calculateTotalPrice();
-	//	var totalPriceElement = document.getElementById("total-price");
-	//	totalPriceElement.textContent = this.totalPrice;
-	//}	
-}
+var myDateElement= document.getElementById("dateTime");
+var myDate = new Date(); 
+ 
+  
+	if (myDate.getHours() < 12 )  
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Morning,"
+} 
+		else 
+	if (myDate.getHours() >= 12 && myDate.getHours() <= 17 ) 
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Afternoon,"
+} 
+		else  
+	if (myDate.getHours() > 17 && myDate.getHours() <= 24 ) 
+{ 
+    var myDateElement = document.getElementById("dateTime"); 
+    myDateElement.textContent="Good Evening,"
+} 
+		else 
+{ 
+    document.write("I'm not sure what time it is!"); 
+} 
 
 
-shop.displayCustomerName();
-shop.displayTotalPrices();
-shop.displayProductList();
+var product =[
+	"Brooklyn T-shirt White",
+	"Brooklyn T-shirt Black",
+	"Apple Watch",
+	"Android Phone"
+];
+var prices = [10,10,199,159];
+var productText ="";
+var discountPrice = 0
+var productElement = document.getElementById("product-list");
+
+productText += "<li class='list-group-item'><span class='badge'>$" + prices[0]+"</span>" + product[0]+"</li>";
+productText += "<li class='list-group-item'><span class='badge'>$" + prices[1]+"</span>" + product[1]+"</li>";
+productText += "<li class='list-group-item'><span class='badge'>$" + prices[2]+"</span>" + product[2]+"</li>";		
+productText += "<li class='list-group-item'><span class='badge'>$" + prices[3]+"</span>" + product[3]+"</li>";
+
+productElement.innerHTML = productText;
+
+totalPrice = prices[0]+prices[1]+prices[2]+prices[3];
+discountPrice = totalPrice*0.75
+
+
+
+//var totalPriceElement = document.getElementById("total-price");
+//totalPriceElement.textContent=totalPrice;
+
+var discountPriceElement = document.getElementById("discount-price");
+discountPriceElement.textContent=discountPrice;
+
+
+var customerName="Pawin";
+var price= 10;
+var quantity= 2;
+
+var customerElement = document.getElementById("customer-name");
+customerElement.textContent = customerName;
